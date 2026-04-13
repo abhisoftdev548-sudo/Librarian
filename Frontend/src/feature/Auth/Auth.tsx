4e4f30cb2d7837a5a48a9224ed167f0e8521739f
Abhishek Gupta <abhisoftdev548@gmail.com>
2026-04-13 21:21:54 +0530

    frontend has started and auth landing page is in designing

---
+++ Frontend/src/feature/Auth/Auth.tsx
@@ -0,0 +1,63 @@
+import React from 'react'
+
+/*************  ✨ Windsurf Command ⭐  *************/
+/**
+ * A component for handling authentication
+ * @returns {React.ReactElement} A React element
+ */
+/*******  6b4bb2ce-ccc9-48f3-a4ee-1258000a5677  *******/
+const Auth = ()   => {
+  return (
+    <div className='w-[90%] mx-auto py-5'>
+      <nav className='flex justify-between items-center px-5'>
+        <div className='w-full text-3xl font-bold text-[#0F172A]'>
+          Librarian
+        </div>
+        <div className='w-full'>
+          <ul className='text-[#475569] flex w-full justify-center gap-8 font-medium text-sm'>
+            <li>About</li>
+            <li>Feature</li>
+          </ul>
+        </div>
+        <div className='flex w-full justify-end gap-8 font-medium text-[14px]'>
+          <button className='bg-[#4F46E5] text-white py-2 rounded-md text-center px-4'>Get Started</button>
+          <button className='border border-[#E2E8F0] py-2 rounded-md text-center px-4 hover:border-[#8bbaf3]'>Log In</button>
+        </div>
+      </nav>
+      <section className='flex justify-between mt-16'>
+        <div className='flex flex-col gap-8'>
+
+        <div className='px-3 py-1 text-[14px] border rounded-2xl border-[#4F46E5] w-fit font-bold bg-[#4e46e58b] text-[#4F46E5] '>
+          Your digital knowledge hub
+        </div>
+        <div>
+          <span className='text-6xl font-black flex flex-col gap-2'><span>CREATE AND LEARN</span> <span>FROM YOUR OWN</span>   <span className='text-[#4F46E5]'>DIGITAL LIBRARY</span></span>
+        </div>
+        <div className='text-lg text-wrap max-w-xl'>
+          Create, organize, and share your knowledge. Store files, notes, and sensitive data in one secure place. Learn without distraction.
+        </div>
+         <div className='flex w-full justify-center gap-8  max-w-sm font-medium text-lg'>
+          <button className='bg-[#4F46E5] text-white py-3 rounded-md text-center w-full '>Get Started</button>
+          <button className='border border-[#E2E8F0] py-3 rounded-md text-center w-full  hover:border-[#8bbaf3]'>Log In</button>
+        </div>
+        <div>
+          <ul className='flex  gap-4'>
+            <li>Notes & Files</li>
+            <li>Encrypted Lockers</li>
+            <li>Course Builder</li>
+          </ul>
+        </div>
+        </div>
+        <div>
+          <div  className='animate-float  drop-shadow-2xl drop-shadow-blue-200 relative'>
+          <div className='absolute px-3 py-1 rounded-2xl border border-[#7e7ec8] text-[14px] font-bold bg-[#1b1b1e8b] text-white top-20 right-0'>No Distraction</div>
+          <img src="./digital-image.png" alt=""  />
+          </div>
+            
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default Auth
