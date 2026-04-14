832634c6f1cd9fb07f53cfdcefd9579bd02c1c60
Abhishek Gupta <abhisoftdev548@gmail.com>
2026-04-14 07:45:55 +0530

    build command and runned first time

--- Frontend/src/feature/Auth/Auth.tsx
+++ Frontend/src/feature/Auth/Auth.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 
 /*************  ✨ Windsurf Command ⭐  *************/
 /**
@@ -13,25 +13,25 @@ const Auth = ()   => {
         <div className='w-full text-3xl font-bold text-[#0F172A]'>
           Librarian
         </div>
-        <div className='w-full'>
+        <div className='w-full items-center'>
           <ul className='text-[#475569] flex w-full justify-center gap-8 font-medium text-sm'>
             <li>About</li>
             <li>Feature</li>
           </ul>
         </div>
-        <div className='flex w-full justify-end gap-8 font-medium text-[14px]'>
+        <div className='flex w-full justify-end gap-8 font-medium text-[14px] items-center'>
           <button className='bg-[#4F46E5] text-white py-2 rounded-md text-center px-4'>Get Started</button>
           <button className='border border-[#E2E8F0] py-2 rounded-md text-center px-4 hover:border-[#8bbaf3]'>Log In</button>
         </div>
       </nav>
-      <section className='flex justify-between mt-16'>
+      <section className='flex flex-col md:flex-row md:justify-between mt-16 justify-center md:items-center'>
         <div className='flex flex-col gap-8'>
 
         <div className='px-3 py-1 text-[14px] border rounded-2xl border-[#4F46E5] w-fit font-bold bg-[#4e46e58b] text-[#4F46E5] '>
           Your digital knowledge hub
         </div>
         <div>
-          <span className='text-6xl font-black flex flex-col gap-2'><span>CREATE AND LEARN</span> <span>FROM YOUR OWN</span>   <span className='text-[#4F46E5]'>DIGITAL LIBRARY</span></span>
+          <span className='text-3xl sm:text-4xl md:text-6xl font-black flex flex-col gap-2'><span>CREATE AND LEARN</span> <span>FROM YOUR OWN</span>   <span className='text-[#4F46E5]'>DIGITAL LIBRARY</span></span>
         </div>
         <div className='text-lg text-wrap max-w-xl'>
           Create, organize, and share your knowledge. Store files, notes, and sensitive data in one secure place. Learn without distraction.
@@ -50,7 +50,7 @@ const Auth = ()   => {
         </div>
         <div>
           <div  className='animate-float  drop-shadow-2xl drop-shadow-blue-200 relative'>
-          <div className='absolute px-3 py-1 rounded-2xl border border-[#7e7ec8] text-[14px] font-bold bg-[#1b1b1e8b] text-white top-20 right-0'>No Distraction</div>
+          <div className='absolute px-3 py-1 rounded-2xl border border-[#7e7ec8] text-[14px] font-bold bg-[#1b1b1e8b] text-white top-20 -right-1 flex items-center gap-2'><span className='min-w-2 min-h-2 rounded-full bg-green-500 inline-block'></span><span>No Distraction</span></div>
           <img src="./digital-image.png" alt=""  />
           </div>
             
